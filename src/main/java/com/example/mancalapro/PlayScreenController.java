67707fa16b7c65a067540dc31e3c951a66fb99ab
Oladepo Olabayoji <bayoji7@gmail.com>
2023-04-27 14:20:39 +0100

    play screen fix

--- src/main/java/com/example/mancalapro/PlayScreenController.java
+++ src/main/java/com/example/mancalapro/PlayScreenController.java
@@ -5,13 +5,16 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -24,6 +27,9 @@ public class PlayScreenController implements Initializable {
     private AnchorPane rootPane;
     @FXML
     private ImageView btnMainMenu;
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -36,8 +42,12 @@ public class PlayScreenController implements Initializable {
                 alert.setContentText("Are you sure you want to end the game?");
                 Optional<ButtonType> result = alert.showAndWait();
                 if (result.isPresent() && result.get() == ButtonType.OK) {
-                    Parent newPane = FXMLLoader.load(getClass().getClassLoader().getResource("view/MainMenu.fxml"));
-                    rootPane.getChildren().setAll(newPane);
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+                    root = loader.load();
+                    stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+                    scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
                 }
             } catch (/* InterruptedException | */IOException e) {
                 throw new RuntimeException(e);
